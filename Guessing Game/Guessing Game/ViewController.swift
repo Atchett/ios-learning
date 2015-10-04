9a099d4e047654b69703f71ed80a8e6c43800e7d
John Spurgin <johnspurgin@gmail.com>
2015-10-04 16:35:35 +0100

    Guessing Game created and fixed

--- Guessing Game/Guessing Game/ViewController.swift
+++ Guessing Game/Guessing Game/ViewController.swift
@@ -11,18 +11,35 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var numberGuess: UITextField!
-    
+    @IBOutlet weak var lblAnswer: UILabel!
     @IBOutlet weak var guessImg: UIImageView!
+    @IBOutlet weak var guessButton: UIButton!
     
     @IBAction func guessClick(sender: AnyObject) {
     
+        // get a random number
+        let rndNumber:Int = Int(arc4random_uniform(5) + 1)
+        // get the guessed number
+        let iGuessed = Int(numberGuess.text!)!
         
+        if iGuessed == rndNumber {
+            guessImg.image = UIImage(named: "good.jpg")
+        } else {
+            guessImg.image = UIImage(named: "bad.jpg")
+            lblAnswer.text = "Answer was : \(rndNumber)"
+        }
+        
+        guessButton.hidden = true
         
     }
     
     @IBAction func tryAgainClick(sender: AnyObject) {
     
-    
+        guessButton.hidden = false
+        guessImg.image = UIImage(named: "question-mark-nothing.jpg")
+        lblAnswer.text = ""
+        numberGuess.text = ""
+        
     }
     
     
