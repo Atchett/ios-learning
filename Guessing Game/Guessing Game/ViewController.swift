18835c18b6bf8c4493d8fb72cb5a1ef87f76ef39
John Spurgin <johnspurgin@gmail.com>
2015-10-04 13:58:06 +0100

    Initial commit of guessing game

---
+++ Guessing Game/Guessing Game/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Guessing Game
+//
+//  Created by John Spurgin on 04/10/2015.
+//  Copyright © 2015 JRS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var numberGuess: UITextField!
+    
+    @IBAction func guessClick(sender: AnyObject) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
